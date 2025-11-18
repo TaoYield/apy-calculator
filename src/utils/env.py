6d30537059e514461a06160fba346cb2164e22ec
Yaroslav Sedyukov <sedyukovofficial@gmail.com>
2025-11-18 21:10:34 +0700

    fix: env

--- src/utils/env.py
+++ src/utils/env.py
@@ -3,8 +3,7 @@ import os
 OTF_ARCHIVE_NODE = "wss://archive.chain.opentensor.ai:443"
 
 def parse_env_data():
-    node = "ws://37.27.64.231:9944"
-    # node = "wss://archive.chain.opentensor.ai:44"
+    node = os.getenv("NODE") or OTF_ARCHIVE_NODE
     batch_size = os.getenv("BATCH_SIZE") or 100
     use_inherited_filter = os.getenv("INHERITED", 'False').lower() in ('true', '1', 't') or False
     no_filters = os.getenv("NO_FILTERS", 'False').lower() in ('true', '1', 't')
